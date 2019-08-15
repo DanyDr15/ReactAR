de24457ab6228a05410ea9f172d142ed0125e21a
DanyDr15 <dapianchiche@gmail.com>
2019-08-15 14:59:40 -0500

    agregando archivos path

--- src/components/ar.js
+++ src/components/ar.js
@@ -3,7 +3,11 @@ import {AFrameRenderer,Marker} from 'react-web-ar'
 export default function ar() {
     return (
         <AFrameRenderer arToolKit={{ sourceType: 'webcam' }} >
-        <Marker parameters={{ preset: 'hiro' }}>
+         <Marker parameters={{
+                preset: "pattern",
+                type: "pattern",
+                url: "https://github.com/DanyDr15/ReactAR/blob/master/src/components/path/pattern-marker.patt"
+              }}>
         
         < a-text  value = " Profesor(a): Susana " > </ a-text ><br></br>
 
